476341eb078b5ddf32847b5a1549695eb61eb6ef
bayram-aloware <bayram@aloware.com>
2022-02-18 16:45:40 +0300

    Fixed a typo

--- config/horizon-wildcard-consumer.php
+++ config/horizon-wildcard-consumer.php
@@ -5,6 +5,6 @@ return [
         // in seconds
         'timeout' => env('QUEUE_OBSERVER_TIMEOUT', 60),
     ],
-    'redis_queue_name_prefix' => env('REDIS_QUEUE_NAME_PREFIX', laravel_database_queues),
+    'redis_queue_name_prefix' => env('REDIS_QUEUE_NAME_PREFIX', 'laravel_database_queues'),
     'rabbitmq_api_url' => env('RABBITMQ_API_URL', 'http://localhost:15672')
 ];
