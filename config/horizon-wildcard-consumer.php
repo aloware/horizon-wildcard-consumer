a0785a8cd4e256ce539bfdcfc48b55d8b5bff70a
bayram-aloware <bayram@aloware.com>
2022-02-23 17:24:12 +0300

    Prefix isn't required anymore

--- config/horizon-wildcard-consumer.php
+++ config/horizon-wildcard-consumer.php
@@ -2,9 +2,7 @@
 
 return [
     'observer' => [
-        // in seconds
-        'timeout' => env('QUEUE_OBSERVER_TIMEOUT', 60),
+        'timeout' => env('QUEUE_OBSERVER_TIMEOUT', 60), // in seconds
     ],
-    'redis_queue_name_prefix' => env('REDIS_QUEUE_NAME_PREFIX', 'laravel_database_queues'),
     'rabbitmq_api_url' => env('RABBITMQ_API_URL', 'http://localhost:15672')
 ];
